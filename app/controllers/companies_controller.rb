db51519a9201b05596c0da523755e9d8826b9978
Lucyna Frank <lucyna@macbook-lucyna-frank.local>
2010-10-26 22:28:45 +0200

    now I can delete company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -27,6 +27,9 @@ class CompaniesController < ApplicationController
    end
   
   def destroy
+    company = Company.find(params[:id])
+    company.destroy
+    redirect_to companies_path
   end
 
 	
